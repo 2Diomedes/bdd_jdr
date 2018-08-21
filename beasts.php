f6c293f159e94a6c99abbe521bce6f611cf18f05
2Diomedes <t.martinteyssier@gmail.com>
2018-08-21 22:02:55 +0200

    création de la page beasts.php et modification de la liste de la nav pour une meilleurs ergonomie

--- beasts.php
+++ beasts.php
@@ -8,7 +8,7 @@
 
   <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch|Special+Elite" rel="stylesheet">
 
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style_beast.css">
   <title>BDD JDR</title>
 </head>
 
@@ -18,17 +18,40 @@
   require "logs.php";
   $bdd = new PDO('mysql:host=localhost;dbname=Bestiaire', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-  $all = $bdd->query("SELECT * FROM beast")->fetchAll();
+  $beast = $bdd->query("SELECT * FROM beast")->fetchAll();
     ?>
 
   <div class="container">
 
-    <?php require "head.php" ?>
+    <header>
+      <nav class="nested">
+        <img src="img/logo_beasts.png" alt="img/logo_beasts.png">
+        <h1><a href="index.php">Bestiaire</a></h1>
+
+        <ul class="ulFlex">
+          <a href="index.php"><li>Acceuil</li></a>
+          <a href="#"><li>Création de compte</li></a>
+          <li class="rollContainer">
+            Catégories
+            <ul class="roll">
+              <a href="beasts.php"><li>Bêtes & Animaux</li></a>
+            </ul>
+          </li>
+          <a href="#"><li>Autre li</li></a>
+        </ul>
+
+        <aside class="account">
+          <img src="https://via.placeholder.com/150x150" alt="avatar">
+          <h3>Your Username</h3>
+          <h3>Rank: ???</h3>
+        </aside>
+      </nav>
+    </header>
 
     <main class="nested">
 
 
-        <?php foreach ($all as $value):?>
+        <?php foreach ($beast as $value):?>
       <a class="cardContainer" href="#">
         <article class="card">
           <img src="<?php echo $value['thumb']; ?>" alt="img/logo.png">
--- head.php
+++ head.php
@@ -1,17 +1,18 @@
 <header>
   <nav class="nested">
     <img src="img/logo.png" alt="img/logo.png">
-    <h1>Bestiaire</h1>
+    <h1><a href="index.php">Bestiaire</a></h1>
 
     <ul class="ulFlex">
-      <li><a href="#">Création de compte</a></li>
+      <a href="index.php"><li>Acceuil</li></a>
+      <a href="#"><li>Création de compte</li></a>
       <li class="rollContainer">
-        <a href="#">Catégories</a>
+        Catégories
         <ul class="roll">
-          <li><a href="#">Bêtes & Animaux</a></li>
+          <a href="beasts.php"><li>Bêtes & Animaux</li></a>
         </ul>
       </li>
-      <li><a href="#">Autre li</a></li>
+      <a href="#"><li>Autre li</li></a>
     </ul>
 
     <aside class="account">
